0e82feab5580ed87407465568a8022ef040ee295
pinchan1213 <hello.tra.fujino@gmail.com>
2024-03-22 14:42:07 +0900

    3/22

--- js/app.js
+++ js/app.js
@@ -9,3 +9,32 @@ $(document).ready(function(){
 	$('.formText')
 })
 
+//バリデーションチェック
+document.getElementById('formSubmit').addEventListener('submit',function(event){
+  //バリデーションチェック
+
+// 変数の定義
+var check = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let validation = true;
+
+  if(name === ""){
+    document.getElementById('message').innerHTML = "名前を入力して下さい";
+    validation = false;
+  } else{
+    document.getElementById('message').innerHTML = "";
+  }
+
+  if(!check.test(email)){
+    document.getElementById('message1').innerHTML = "メールアドレスの形式で入力して下さい";
+    validation = false;
+  }else{
+    document.getElementById('message1').innerHTML = "";
+  }
+
+  // バリデーションに引っかかった場合は送信させない
+  if(!validation){
+    event.preventDefault();
+  }
+});
